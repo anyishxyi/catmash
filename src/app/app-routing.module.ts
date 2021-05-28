d4db992a6b34907e1a0a485d61a037d8bc80e46b
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2021-05-28 20:12:42 +0200

    adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VoteComponent } from './vote/vote.component';
+import { RakingComponent } from './raking/raking.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'vote', component: VoteComponent },
+  { path: 'raking', component: RakingComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
