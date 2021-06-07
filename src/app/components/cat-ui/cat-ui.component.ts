1654589e3f42a849d720d6ae34776aa2e0364082
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2021-06-07 17:57:59 +0200

    rm onchanges hook

--- src/app/components/cat-ui/cat-ui.component.ts
+++ src/app/components/cat-ui/cat-ui.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-cat-ui',
   templateUrl: './cat-ui.component.html',
   styleUrls: ['./cat-ui.component.scss']
 })
-export class CatUiComponent implements OnChanges {
+export class CatUiComponent {
   @Input() catData: any;
   @Output() onSelectedCat = new EventEmitter<number>();
-
-  ngOnChanges(): void {}
 
   onSelect(value: number): void {
     this.onSelectedCat.emit(value);
